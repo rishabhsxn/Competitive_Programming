05073ae4d465965ff01daa04dc14d3e6341451bf
Vold <rishabhsaxena1998@gmail.com>
2020-03-04 00:27:28 +0530

    Save_the_Prisoner completed on try_2

--- hackerrank/Save_the_Prisoner.py
+++ hackerrank/Save_the_Prisoner.py
@@ -2,10 +2,27 @@
 # start distribution from that position and continue till the candies finish (distribute again in circle if candies are left)
 # the last candy is aweful, warn the prisoner who will be receiving that candy
 
-prisoners = 7
-candies = 19
-startingPosition = 2
+# 999999999 999999999 1
+# 352926151 380324688 94730870
+# 499999999 999999998 2
+# 499999999 999999997 2
+# 210041918 328800789 159203369
 
+prisoners = 210041918
+candies = 328800789
+startingPosition = 159203369
+
+
+def saveThePrisoner2(prisoners, candies, startingPosition):
+
+    unluckyPrisoner = (candies % prisoners) + (startingPosition - 1)
+
+    if(unluckyPrisoner>prisoners):
+        unluckyPrisoner = unluckyPrisoner % prisoners      # because after adding from where we started, the result may become more than no. of prisoners
+
+    if(unluckyPrisoner==0):
+        return prisoners
+    return unluckyPrisoner
 
 def saveThePrisoner(prisoners, candies, startingPosition):
 
@@ -28,5 +45,5 @@ def saveThePrisoner(prisoners, candies, startingPosition):
 
     return unluckyPrisoner
 
-result = saveThePrisoner(prisoners, candies, startingPosition)
+result = saveThePrisoner2(prisoners, candies, startingPosition)
 print(result)
