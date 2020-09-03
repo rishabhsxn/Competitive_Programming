be08b500ba81e75f109fafd22872adc58caa42b8
Vold <rishabhsaxena1998@gmail.com>
2020-09-03 22:28:23 +0530

    Trapping Rain Water - Implement Method #2a - Time O(n), Space O(n)

--- misc/Trapping_Rain_Water.cpp
+++ misc/Trapping_Rain_Water.cpp
@@ -6,13 +6,16 @@ Output: 2
 
 solution: https://www.geeksforgeeks.org/trapping-rain-water/
 
-solution #1: A block (element in array) can hold water if there is a left wall AND a right wall at any distance from them.
+METHOD #1: A block (element in array) can hold water if there is a left wall AND a right wall at any distance from them.
 So, for every element, iterate left and right and find highest walls. If any of these walls are not taller than the block itself, 
 then it can't hold water. If both are taller, then the block will hold water drops equal to minimum of height differences of left 
 and right wall w.r.t. to the block.
 Time complexity: O(n^2)
 Space complexity: O(1)
 
+METHOD #2: (a) Instead of finding left and right wall while iterating the blocks, find the lmax and rmax before iterating and save them
+in two different arrays.
+(b) It can be further improved by using lmax and rmax variables instead of array
 
 */
 
@@ -21,6 +24,7 @@ Space complexity: O(1)
 using namespace std;
 
 int calculateTotalWater(int size, int blocks[]);
+int calculateTotalWater2a(int size, int blocks[]);
 
 int main(){
     int size;
@@ -34,7 +38,7 @@ int main(){
     for(int i=0; i<size; i++)
         cin >> blocks[i];
 
-    int waterDrops = calculateTotalWater(size, blocks);
+    int waterDrops = calculateTotalWater2a(size, blocks);
     cout << "\nTotal water drops: " << waterDrops << endl << endl;
 
 
@@ -69,3 +73,25 @@ int calculateTotalWater(int size, int blocks[]){
     return water;
 }
 
+int calculateTotalWater2a(int size, int blocks[]){
+    int leftMax[size], rightMax[size], water=0;
+
+    leftMax[0] = blocks[0];
+    rightMax[size-1] = blocks[size-1];
+
+    // find highest left wall for every block
+    for(int i=1; i<size; i++)
+        leftMax[i] = max(leftMax[i-1], blocks[i]);
+
+    // find highest right wall for every block
+    for(int j=size-2; j>=0; j--)
+        rightMax[j] = max(rightMax[j+1], blocks[j]);
+
+    // find water stored using above left and right wall data
+    for(int k=0; k<size; k++){
+        if(leftMax[k]>blocks[k] && rightMax[k]>blocks[k])
+            water += min(leftMax[k], rightMax[k]) - blocks[k];
+    }
+
+    return water;
+}
